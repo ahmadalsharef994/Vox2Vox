bd53c9636bda309d418fa9728d26f8559d62bb9f
ahmadalsharef <ahmad.alsharef@medzgo.com>
2022-05-03 23:27:37 +0530

    train fix

--- train_v2v.py
+++ train_v2v.py
@@ -20,6 +20,7 @@ D = Discriminator()
 generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
+
 @tf.function
 def train_step(image, target, alpha):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
@@ -29,17 +30,22 @@ def train_step(image, target, alpha):
         disc_real_output = D([image, target], training=True)
         disc_fake_output = D([image, gen_output], training=True)
         disc_loss = discriminator_loss(disc_real_output, disc_fake_output)
-        
-        gen_loss, dice_loss, disc_loss_gen = generator_loss(target, gen_output, disc_fake_output, class_weights, alpha)
+
+        gen_loss, dice_loss, disc_loss_gen = generator_loss(
+            target, gen_output, disc_fake_output, class_weights, alpha)
 
     generator_gradients = gen_tape.gradient(gen_loss, G.trainable_variables)
-    discriminator_gradients = disc_tape.gradient(disc_loss, D.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(
+        disc_loss, D.trainable_variables)
 
-    generator_optimizer.apply_gradients(zip(generator_gradients, G.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(discriminator_gradients, D.trainable_variables))
-        
+    generator_optimizer.apply_gradients(
+        zip(generator_gradients, G.trainable_variables))
+    discriminator_optimizer.apply_gradients(
+        zip(discriminator_gradients, D.trainable_variables))
+
     return gen_loss, dice_loss, disc_loss_gen
-        
+
+
 @tf.function
 def test_step(image, target, alpha):
     gen_output = G(image, training=False)
@@ -48,29 +54,31 @@ def test_step(image, target, alpha):
     disc_fake_output = D([image, gen_output], training=False)
     disc_loss = discriminator_loss(disc_real_output, disc_fake_output)
 
-    gen_loss, dice_loss, disc_loss_gen = generator_loss(target, gen_output, disc_fake_output, class_weights, alpha)
-        
-    return gen_loss, dice_loss, disc_loss_gen # TC_loss, WT_loss, ET_loss, IOU_mean
+    gen_loss, dice_loss, disc_loss_gen = generator_loss(
+        target, gen_output, disc_fake_output, class_weights, alpha)
+
+    return gen_loss, dice_loss, disc_loss_gen  # TC_loss, WT_loss, ET_loss, IOU_mean
+
 
 def fit(train_gen, valid_gen, alpha, epochs):
-    
-    path = './RESULTS' 
-    if os.path.exists(path)==False:
+
+    path = './RESULTS'
+    if os.path.exists(path) == False:
         os.mkdir(path)
-        
+
     Nt = len(train_gen)
     history = {'train': [], 'valid': []}
     prev_loss = np.inf
-    
+
     epoch_v2v_loss = tf.keras.metrics.Mean()
     epoch_dice_loss = tf.keras.metrics.Mean()
     epoch_disc_loss = tf.keras.metrics.Mean()
     epoch_v2v_loss_val = tf.keras.metrics.Mean()
     epoch_dice_loss_val = tf.keras.metrics.Mean()
     epoch_disc_loss_val = tf.keras.metrics.Mean()
-    
+
     for e in range(epochs):
-        print('Epoch {}/{}'.format(e+1,epochs))
+        print('Epoch {}/{}'.format(e+1, epochs))
         b = 0
         for Xb, yb in train_gen:
             b += 1
@@ -79,54 +87,54 @@ def fit(train_gen, valid_gen, alpha, epochs):
             epoch_dice_loss.update_state(losses[1])
             epoch_disc_loss.update_state(losses[2])
             # TC_loss, WT_loss, ET_loss, IOU_mean
-            dice_necrotic = dice_coef_necrotic(Xb, yb)
-            print("dice_coef_necrotic:"+dice_necrotic)
 
             stdout.write('\rBatch: {}/{} - loss: {:.4f} - dice_loss: {:.4f} - disc_loss: {:.4f} - disc_loss: {:.4f} '
-                         .format(b, Nt, epoch_v2v_loss.result(), epoch_dice_loss.result(), epoch_disc_loss.result(), dice_necrotic))
+                         .format(b, Nt, epoch_v2v_loss.result(), epoch_dice_loss.result(), epoch_disc_loss.result()))
             stdout.flush()
-        history['train'].append([epoch_v2v_loss.result(), epoch_dice_loss.result(), epoch_disc_loss.result()])
-        
+        history['train'].append(
+            [epoch_v2v_loss.result(), epoch_dice_loss.result(), epoch_disc_loss.result()])
+
         for Xb, yb in valid_gen:
             losses_val = test_step(Xb, yb, alpha)
             epoch_v2v_loss_val.update_state(losses_val[0])
             epoch_dice_loss_val.update_state(losses_val[1])
             epoch_disc_loss_val.update_state(losses_val[2])
-            dice_necrotic = dice_coef_necrotic(Xb, yb)
-            print("dice_coef_necrotic:"+dice_necrotic)
-            
+
         stdout.write('\n               loss_val: {:.4f} - dice_loss_val: {:.4f} - disc_loss_val: {:.4f}'
                      .format(epoch_v2v_loss_val.result(), epoch_dice_loss_val.result(), epoch_disc_loss_val.result()))
         stdout.flush()
-        history['valid'].append([epoch_v2v_loss_val.result(), epoch_dice_loss_val.result(), epoch_disc_loss_val.result()])
-        
-        # save pred image at epoch e 
+        history['valid'].append([epoch_v2v_loss_val.result(
+        ), epoch_dice_loss_val.result(), epoch_disc_loss_val.result()])
+
+        # save pred image at epoch e
         y_pred = G.predict(Xb)
         y_true = np.argmax(yb, axis=-1)
         y_pred = np.argmax(y_pred, axis=-1)
 
         canvas = np.zeros((128, 128*3))
         idx = np.random.randint(len(Xb))
-        
-        x = Xb[idx,:,:,64,2] 
+
+        x = Xb[idx, :, :, 64, 2]
         canvas[0:128, 0:128] = (x - np.min(x))/(np.max(x)-np.min(x)+1e-6)
-        canvas[0:128, 128:2*128] = y_true[idx,:,:,64]/3
-        canvas[0:128, 2*128:3*128] = y_pred[idx,:,:,64]/3
-        
+        canvas[0:128, 128:2*128] = y_true[idx, :, :, 64]/3
+        canvas[0:128, 2*128:3*128] = y_pred[idx, :, :, 64]/3
+
         fname = (path + '/pred@epoch_{:03d}.png').format(e+1)
         mpim.imsave(fname, canvas, cmap='gray')
-        
+
         # save models
         print(' ')
-        if epoch_v2v_loss_val.result() < prev_loss:    
-            G.save_weights(path + '/Generator.h5') 
+        if epoch_v2v_loss_val.result() < prev_loss:
+            G.save_weights(path + '/Generator.h5')
             D.save_weights(path + '/Discriminator.h5')
-            print("Validation loss decresaed from {:.4f} to {:.4f}. Models' weights are now saved.".format(prev_loss, epoch_v2v_loss_val.result()))
+            print("Validation loss decresaed from {:.4f} to {:.4f}. Models' weights are now saved.".format(
+                prev_loss, epoch_v2v_loss_val.result()))
             prev_loss = epoch_v2v_loss_val.result()
         else:
-            print("Validation loss did not decrese from {:.4f}.".format(prev_loss))
+            print(
+                "Validation loss did not decrese from {:.4f}.".format(prev_loss))
         print(' ')
-        
+
         # resets losses states
         epoch_v2v_loss.reset_states()
         epoch_dice_loss.reset_states()
@@ -134,7 +142,7 @@ def fit(train_gen, valid_gen, alpha, epochs):
         epoch_v2v_loss_val.reset_states()
         epoch_dice_loss_val.reset_states()
         epoch_disc_loss_val.reset_states()
-        
+
         del Xb, yb, canvas, y_pred, y_true, idx
-        
+
     return history
